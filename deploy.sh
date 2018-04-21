611f50d22d92426f2044952df27cc50e75b0dcd4
Paul <paul-tang@astra.cloud>
2018-04-22 04:44:11 +0800

    [Add] First version of installation script, Python3, dlib, face_recognition, opencv, miniconda

---
+++ deploy.sh
@@ -0,0 +1,65 @@
+# basic lib install 
+printf '\n'
+printf '================ Install essential toolkit via apt-get install ================\n'
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/lib/dpkg/lock
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo apt-get update
+sudo apt-get install -f 
+sudo apt-get install build-essential gcc g++ -y
+sudo apt-get install build-essential cmake -y
+sudo apt-get install libgtk-3-dev -y
+sudo apt-get install libboost-all-dev -y
+sudo apt-get install build-essential cmake pkg-config -y
+sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+sudo apt-get install libatlas-base-dev gfortran -y
+sudo apt-get install libopenblas-dev -y 
+sudo apt-get install liblapack3
+sudo apt-get install git -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-4.9 -y
+sudo apt-get upgrade libstdc++6 -y
+
+
+# miniconda
+printf '================ Install Miniconda3 for Python ================\n'
+sudo rm -r mini*
+sudo rm Mini*
+
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME/miniconda3"
+printf '%s\n' 'export PATH="'$HOME'/miniconda3/bin:$PATH"' >> $HOME/.bashrc
+export PATH="$HOME/miniconda3/bin:$PATH"
+
+printf '================ Install Python lib ================\n'
+conda update -n base conda -y
+pip install --upgrade pip
+pip install --user wget 
+pip install --user  numpy scipy scikit-image scikit-learn redis pymysql flask imageio h5py keras tornado tensorflow slacker setuptools pillow
+pip install --user boto3 requests
+pip install --user logmatic-python
+conda install ipython -y
+conda install -c menpo opencv -y
+conda install mkl -y
+pip install --user colorama 
+conda install pytorch-cpu torchvision -c pytorch -y
+
+
+# build dlib from source 
+printf '================ Install DLIB lib ================\n'
+git clone https://github.com/davisking/dlib.git
+cd dlib
+mkdir build
+cd build
+cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1
+cmake --build .
+cd ..
+python setup.py install 
+pip install --user face_recognition 
+cd ..
---
+++ startup.py
@@ -0,0 +1,141 @@
+import time
+import sys
+import os
+import platform
+
+if sys.version_info[0] >= 3:
+    print('Environment = Python 3 ---> Correct!')
+else:
+    try:
+        print ('Environment = Python 2 ---> Incorrect!')
+        print ('Environment testing fail, stop! ')
+        exit()
+    except:
+        os.system('echo python2 error')
+        exit()
+
+if platform.system() != 'Linux':
+    print('It is not Linux based system ---> Error')
+    exit()
+
+
+print()
+
+print('Checking for Python toolkit...')
+
+print('Checking for numpy')
+try:
+    import numpy as np
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install numpy before continuing")
+    exit()
+
+print('Checking for dlib')
+try:
+    import dlib as d
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install dlib before continuing")
+    exit()
+
+
+print('Checking for opencv')
+try:
+    import cv2 as c
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install opencv before continuing")
+    exit()
+
+print('Checking for skimage')
+try:
+    import skimage as s
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install skimage before continuing")
+    exit()
+
+
+
+print('Checking for platform')
+try:
+    import platform
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install platform before continuing")
+    exit()
+
+
+print('Checking for reload')
+try:
+    from imp import reload
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install reload before continuing")
+    exit()
+
+
+print('Checking for pickle')
+try:
+    import pickle
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install pickle before continuing")
+    exit()
+
+
+print('Checking for Pytorch')
+try:
+    import torch
+    import torch.nn.init as init
+    from torch.autograd import Variable
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install platform before continuing")
+    exit()
+
+print('Checking for face_recognition')
+try:
+    import face_recognition as fr
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install face_recognition before continuing")
+    exit()
+
+
+
+print('Checking for imageio')
+try:
+    import imageio as im
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install imageio before continuing")
+    exit()
+
+print('Checking for sklearn')
+try:
+    from sklearn import datasets, svm, metrics
+    print('---> Correct!')
+except ImportError:
+    print('---> Error!')
+    print("you should install imageio before continuing")
+    exit()
+
+
+print()
+print()
+
+print('Checking Python toolkit finished !')
+
+print('Congratulation, you are ready to go!')
