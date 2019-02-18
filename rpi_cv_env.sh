8f9bab3b4dfbbae0e1784e99be9f1dfd94890161
cftang <cftang@cftangde-MacBook-Pro-2.local>
2019-02-18 15:07:10 +0800

    1. upgrade opencv from 3.2.0 to 3.4.0 2. add libqtgui4 install

--- rpi_cv_env.sh
+++ rpi_cv_env.sh
@@ -9,6 +9,7 @@ sudo apt-get install libgtk2.0-dev -y
 sudo apt-get install cmake -y
 sudo apt-get install libhdf5-serial-dev -y
 sudo apt-get install libhdf5-dev -y
+sudo apt-get install -y libqtgui4
 
 
 sudo apt-get install default-jdk ant -y
@@ -41,18 +42,18 @@ sudo pip3 install keras -i https://www.piwheels.org/simple
 
 
 cd ~
-wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.2.0.zip
-wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.2.0.zip
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.4.0.zip
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.4.0.zip
 unzip opencv.zip
 unzip opencv_contrib.zip
-cd ~/opencv-3.2.0/
+cd ~/opencv-3.4.0/
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D INSTALL_C_EXAMPLES=OFF \
     -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.2.0/modules \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.4.0/modules \
     -D BUILD_EXAMPLES=ON \
     -D ENABLE_NEON=ON ..
 
